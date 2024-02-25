9cb6815d1d187686e9011cca8927983b7f8d0d35
Rahulnarode58 <rahulnarode58@gmail.com>
2024-02-25 08:32:24 +0530

    Reach a given score

---
+++ Reach a_given_score.cpp
@@ -0,0 +1,54 @@
+// <<<<<<<<<<<<<<<<<< Reach a given score <<<<<<<<<<<<<<<<<<
+// POTD Date 25-02-2024
+// Coding_with_rahul
+// Level : easy
+// Topic : Dynamic Programming and Algorithms
+
+/*Consider a game where a player can score 3 or 5 or 10 points in a move. Given a total score n, find number of distinct combinations to reach the given score.
+
+Example 1:
+
+Input
+n = 10
+Output
+2
+Explanation
+There are two ways {5,5} and {10}.*/
+
+/*Your Task:
+You don't need to read input or print anything. Your task is to complete the function count( ) which takes n as input parameter and returns the answer to the problem.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(n)
+
+Constraints:
+
+1 ≤ n ≤ 106*/
+
+class Solution
+{
+public:
+    long long int count(long long int n)
+    {
+        // Your code here
+        std::vector<long> dp(n + 1, 0);
+        dp[0] = 1;
+
+        for (int i = 3; i <= n; ++i)
+        {
+            dp[i] += dp[i - 3];
+        }
+
+        for (int i = 5; i <= n; ++i)
+        {
+            dp[i] += dp[i - 5];
+        }
+
+        for (int i = 10; i <= n; ++i)
+        {
+            dp[i] += dp[i - 10];
+        }
+
+        return dp[n];
+    }
+};
